9bc11e3b15140707f88bab281284e0a29022be2c
HafizIkrashSE <hafizikrash809@gmail.com>
2026-09-08 19:53:29 +0800

    dsa leetcodes

---
+++ DSA with C++/Level 0/0 Simulation - Arrays/1_Running_Sum_of_1d_Array.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+};
